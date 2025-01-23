5071d7052d91bafcc0af027346e6968c6ab63fb6
mikster36 <45michaelo88@gmail.com>
2025-01-22 19:17:19 -0500

    hide personal website for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TwoStepAuthenticationPage from './2sa/TwoStepAuthenticationPage.tsx';
-import AboutMe from "./home/components/AboutMe.tsx";
-import Projects from "./home/components/Projects.tsx";
 import { HelmetProvider } from "react-helmet-async";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,8 +12,7 @@ function App() {
                 <Router>
                     <>
                         <Routes>
-                            <Route path={"/"} element={<AboutMe />} />
-                            <Route path={"/projects"} element={<Projects />}/>
+                            <Route path={"/"} element={<TwoStepAuthenticationPage />} />
                             <Route path={"/two-step-authentication"} element={<TwoStepAuthenticationPage />} />
                         </Routes>
                     </>
