1547c2730e959cb635e886a4f19d621a9363dbcd
mikster36 <45michaelo88@gmail.com>
2026-02-13 19:44:59 -0500

    use reverse proxy

--- src/2sa/constants.ts
+++ src/2sa/constants.ts
@@ -1,4 +1,5 @@
-export const AUDIO_S3_PATH = 'https://2saarchive.s3.amazonaws.com/music/';
-export const TRACKLIST_S3_PATH = 'https://2saarchive.s3.amazonaws.com/tracklist/';
-export const VIDEO_S3_PATH = 'https://2saarchive.s3.amazonaws.com/videos/';
+const MEDIA_BASE_PATH = '/media/'
+export const AUDIO_S3_PATH = `${MEDIA_BASE_PATH}music/`;
+export const TRACKLIST_S3_PATH = `${MEDIA_BASE_PATH}tracklist/`;
+export const VIDEO_S3_PATH = `${MEDIA_BASE_PATH}videos/`;
 export const IMAGE_PATH = '/2sa/pics';
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        proxy: {
+            '/media': {
+                target: 'https://darzonktt5ywx.cloudfront.net',
+                changeOrigin: true,
+                secure: true
+            }
+        }
+    }
 })
